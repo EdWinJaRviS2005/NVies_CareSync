97ea89aa2c7c9dbbd2258c2bbb468b10aaa1ca46
EdWinJaRviS2005 <hrishikeshpatil5002@gmail.com>
2026-04-30 20:18:15 +0530

    changed few things

--- frontend/app/availability/layout.tsx
+++
@@ -1,5 +0,0 @@
-import AuthenticatedLayout from "@/components/AuthenticatedLayout";
-
-export default function Layout({ children }: { children: React.ReactNode }) {
-  return <AuthenticatedLayout>{children}</AuthenticatedLayout>;
-}
